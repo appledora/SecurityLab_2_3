9e5fe0d72eb2c4e767a6b00e1f2f106956645a9d
Sowmen Das <sowmendipta@gmail.com>
2021-04-11 00:03:22 +0600

    add sh

---
+++ Task_3/Problem 5/run_hmac.sh
@@ -0,0 +1,13 @@
+echo "----- Generating Hash ----"
+echo "\nMD5 Key=abcdefg"
+openssl dgst -md5 -hmac "abcdefg" hmac_input.txt
+echo "\nMD5 Key=abcd"
+openssl dgst -md5 -hmac "abcd" hmac_input.txt
+echo "\nSHA256 Key=abcdefg"
+openssl dgst -sha256 -hmac "abcdefg" hmac_input.txt
+echo "\nSHA256 Key=abcd"
+openssl dgst -sha256 -hmac "abcd" hmac_input.txt
+echo "\nSHA1 Key=ab"
+openssl dgst -sha1 -hmac "ab" hmac_input.txt
+echo "\nSHA1 Key=abcdefghijklmnop"
+openssl dgst -sha1 -hmac "abcdefghijklmnop" hmac_input.txt
